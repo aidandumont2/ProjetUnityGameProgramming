9cf3061215063d5c508cd99394945b3ddc0308c1
RFENZL1 <103497453+RFENZL1@users.noreply.github.com>
2023-01-23 12:14:21 +0100

    Menu fait

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("User clicked on Play button");
+
+        SceneManager.LoadScene("SampleScene");
+        //SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("User clicked on Quit button");
+
+        Application.Quit();
+    }
+}
