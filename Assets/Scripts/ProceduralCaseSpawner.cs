9623119df91f0ec323a75f91cadcb55cbab97274
James <61620217+vezinjames@users.noreply.github.com>
2023-01-27 12:20:30 +0100

    Procedural almost finished

---
+++ Assets/Scripts/ProceduralCaseSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralCaseSpawner : MonoBehaviour
+{
+    public GameObject[] Cases;
+    public GameObject Spawned;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x=-4; x<=4; x++)
+        {
+            for (int y=-4; y<=4; y++) {
+                var randomCase = Cases[Random.Range(0, Cases.Length)];
+
+                Instantiate(randomCase, new Vector3(x * 10, y * 5), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
